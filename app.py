fa54fc7e0fb0098700beff24eee01095e72e9f6f
Rugved <74641670+rugvedp@users.noreply.github.com>
2024-04-24 19:47:16 +0530

    updated UI

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template, request
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import pickle
+import json
+from youtubesearchpython import VideosSearch
+
+def search_youtube_videos(topic, max_results=6):
+    videosSearch = VideosSearch(topic, limit=max_results)
+    result = videosSearch.result()
+    videos = []
+    for video in result['result']:
+        video_data = {
+            "title": video['title'],
+            "url": video['link'],
+            "views": video['viewCount']['short'],
+            "duration": video['duration'],
+            "thumbnail": video['thumbnails'][0]['url']
+        }
+        videos.append(video_data)
+    return videos
+
+with open('interest.json', 'r') as f:
+    recommend = json.load(f)
+
 
 model = pickle.load(open('model.pkl', 'rb'))
-
 data = pd.read_csv("students1111.csv")
 le_major = LabelEncoder()
 le_domain = LabelEncoder()
@@ -39,8 +59,17 @@ def predict():
     encoded_projects = le_projects.transform([project])
 
     prediction = model.predict([[0,encoded_domain[0], encoded_projects[0], numericValuePY, numericValueSQL,numericValueJAVA]])   
-
-    return render_template('index2.html', prediction_text= 'Your should choose {}' . format(prediction[0]) ) 
+    enigneer = None
+    for item in recommend:
+        if item['interest'] == prediction[0]:
+            enigneer = item
+            break
+    print(enigneer)
+    
+    
+    search = prediction[0] + 'roadmap'
+    videos = search_youtube_videos(search)
+    return render_template('index2.html',videos=videos, data=item) 
     
 
 if __name__ == '__main__':
